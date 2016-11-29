1026ed4e5f1e732eff94205b36970b240e9b6f8d
loics_000 <loics_000@LENOVO-PC>
2016-11-29 09:25:40 +0100

    ADD dto and start DAL singleton

---
+++ Smart-Video/DAL/DAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class DAL
+    {
+        public static DAL ContextDal=new DAL();
+        private FilmBDDataContext dbDataContext;
+
+        private DAL()
+        {
+            dbDataContext = new FilmBDDataContext();
+        }
+
+        public List<Film> SelectAllFilmObject()
+        {
+            return dbDataContext.Films.ToList();
+        }
+    }
+}
